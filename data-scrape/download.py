f210e16bb8aad591a1d05ad87d726cc41c21b9f6
Kol Crooks <kolcrooks10@gmail.com>
2022-01-15 13:23:31 -0800

    removed venv file and finished scaping code

--- data-scrape/download.py
+++ data-scrape/download.py
@@ -10,9 +10,26 @@ after = int(dt.datetime(2020, 1, 1).timestamp())
 subreddit = "LegalAdvice"
 limit = 100
 
-_subs = api.search_submissions(subreddit=subreddit, before=before, after=after)
-subs = [s for s in _subs]
-print(subs[0])
+submission_gen = api.search_submissions(
+    subreddit=subreddit, before=before, after=after, score='=>19')
 
-# dataframe = pd.DataFrame(comments)
-# dataframe.to_csv('./comments.csv', header=True, index=False, columns=list(dataframe.axes[1]))
+subs = []
+for s in submission_gen:
+
+    try:
+        subs.append({'id': s['id'], 'title': s['title'], 'body': s['selftext'],
+                    'score': s['score']})
+    except:
+        pass
+
+    if len(subs) == 100:
+        for s in subs:
+            ids = [i for i in api.search_submission_comment_ids(
+                ids=s['id'])]
+            s['comments'] = [c for c in api.search_comments(
+                ids=ids, filterFn=lambda c: c['link_id'][:2] == 't3')]
+
+        df = pd.DataFrame(subs)
+        df.to_csv(f'./submissions/{dt.datetime.now()}.csv', header=True, index=False,
+                  columns=list(df.axes[1]))
+        subs = []
