f83f51a193fea927128991ef0643663b9a98d902
Lance Tan <ltan0521@gmail.com>
2022-01-16 02:06:27 -0800

    Added disclaimer and contributions

--- web/src/landing.js
+++ web/src/landing.js
@@ -38,15 +38,22 @@ export default function Landing() {
     }
 
     return (
-        <div className="Landing flex flex-col justify-center items-center min-h-full min-w-full transition-all">
-            <div className="py-1 m-0 font-serif text-5xl text-left tracking-wide">
-                AdvocAI
+        <div className="Landing flex flex-col min-h-full min-w-full">
+            <div className="flex flex-col justify-center items-center grow transition-all">
+                <div className="py-1 m-0 font-serif text-5xl text-left tracking-wide">
+                    AdvocAI
+                </div>
+                <div className="py-0 m-0 font-serif text-2xl italic">
+                    Your AI Lawyer.
+                </div>
+                <div className="pt-3">
+                    <button className="font-serif bg-black text-white rounded-md p-2" onClick={()=>setState(1)}>Help me.</button>
+                </div>
             </div>
-            <div className="py-0 m-0 font-serif text-2xl italic">
-                Your AI Lawyer.
+            <div>
+                <div className="flex alight-left justify-left pb-5 pl-5">A project by Team Valley</div>
+                <div className="flex text-xs align-text-bottom justify-center pb-2">Disclaimer: The information provided on this website does not, and is not intended to, constitute legal advice.</div>    
             </div>
-            <button className="font-serif bg-black text-white rounded-md p-2" onClick={()=>setState(1)}>Help me.</button>
-            {get_messages()}
         </div>
       )
 }
